4b1963083201d8a0ad56f285ee2188484b3bc8e0
TraFost <rahwisdilfiqrak@gmail.com>
2025-02-22 03:23:26 +0700

    chore: update import statements and refine file inclusion patterns in biome configuration

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import type { ComponentProps, ElementType } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { type VariantProps, cva } from "class-variance-authority";
+import type { ComponentProps, ElementType } from "react";
 
 import { cn } from "lib/utils";
 
