a122fb9d23ff22ba740a8aea6dc569ac85b7c199
TraFost <rahwisdilfiqrak@gmail.com>
2025-02-22 02:49:15 +0700

    feat: add configuration files for Biome

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "components/ui/button";
 
 export default async function Page() {
-	return (
-		<Button variant="outline" size="lg">
-			Click me
-		</Button>
-	);
+  return (
+    <Button variant="outline" size="lg">
+      Click me
+    </Button>
+  );
 }
