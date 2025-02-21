d12dead5e4194c3ff89f59c4f84870c2d67538d4
TraFost <rahwisdilfiqrak@gmail.com>
2025-02-22 03:09:45 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "components/ui/button";
 export default async function Page() {
   return (
     <Button variant="outline" size="lg">
-      Click mes
+      Click me
     </Button>
   );
 }
