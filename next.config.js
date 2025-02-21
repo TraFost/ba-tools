24f08e5950f97a0e7bcd2b466e21e0239d51d6fd
TraFost <rahwisdilfiqrak@gmail.com>
2025-02-22 02:50:41 +0700

    chore: remove comment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
 	reactStrictMode: false,
-	
+
 };
 
 module.exports = nextConfig;
