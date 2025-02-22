da35baf73caf1d7a55d4d55c1ca8575c84fdf1a2
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-02-22 23:43:56 +0700

    fix: tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function Page() {
   return (
     <div className="size-full flex items-center justify-center">
       <p>Blue archive tools</p>
+      <Button>button</Button>
     </div>
   );
 }
