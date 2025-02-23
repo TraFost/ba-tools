6a041a7dee9012e1d43f6ffc3a06d8c8996ade4f
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-02-24 03:09:15 +0700

    style: page layout and color variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import "./styles/globals.css";
+import { Open_Sans } from "next/font/google";
+
+const open_sans = Open_Sans({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +13,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className="min-h-full h-dvh min-w-full">{children}</main>
+        <main
+          className={`min-h-full h-dvh min-w-full ${open_sans.className} bg-cover bg-center bg-[url(/hexa_back_01.png)]`}
+        >
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import { Button } from "./components/ui/button";
+import BigCard from "components/ui/big-card";
 
 export default function Page() {
   return (
-    <div className="size-full flex items-center justify-center">
-      <p>Blue archive tools</p>
-      <Button>button</Button>
+    <div className="size-full flex flex-col gap-8 sm:gap-24 px-8 max-sm:pt-12 sm:items-center sm:justify-center">
+      <div className="flex flex-col items-center">
+        <p className="text-2xl font-bold text-secondary-foreground">Blue Archive Tools</p>
+        <p className="text-lg text-center font-semibold text-accent-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quod placeat possimus
+          dolorum nesciunt ab esse eveniet at!
+        </p>
+      </div>
+      <div className="flex flex-wrap h-fit gap-x-8 gap-y-4 sm:gap-12 justify-center">
+        <BigCard href="#" bgUrl="/BG.jpg">
+          Dialog Maker
+        </BigCard>
+        <BigCard href="#" bgUrl="/BG_HyakkiyakoSquareStreet.jpg">
+          Momotalk Generator
+        </BigCard>
+        <BigCard href="#" bgUrl="/BG_SchaleOperationRoom.jpg">
+          Music Archive
+        </BigCard>
+      </div>
     </div>
   );
 }
