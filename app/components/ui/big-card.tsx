636646bbe03e0190210d7a40725894a1971e94b3
TraFost <rahwisdilfiqrak@gmail.com>
2025-02-24 19:45:40 +0700

    feat: refactor BigCard component to use CustomLink and improve props handling

--- app/components/ui/big-card.tsx
+++ app/components/ui/big-card.tsx
@@ -1,20 +1,25 @@
-import Link from "next/link";
+import type { ReactNode, FC } from "react";
+import { Link as CustomLink } from "components/ui/link";
 
-export const BigCard = ({
-  children,
-  href,
-  bgUrl,
-}: { children: React.ReactNode; href: string; bgUrl: string }) => {
-  return (
-    <Link
-      href={href}
-      style={{ backgroundImage: `url(${bgUrl})` }}
-      className="bg-cover bg-center text-secondary-foreground w-48 h-52 rounded-md -skew-x-6 relative py-4 px-3 hover:outline-4 hover:outline-accent-foreground"
-    >
-      <div className="bg-gradient-to-b from-white from-0% via-50% via-white/50 to-transparent to-75% size-full absolute inset-0 rounded-md" />
-      <p className="text-2xl font-bold skew-x-6 italic w-fit">{children}</p>
-    </Link>
-  );
+interface Props {
+	children: ReactNode;
+	href: string;
+	bgUrl: string;
+}
+
+export const BigCard: FC<Props> = (props) => {
+	const { bgUrl, children, href } = props;
+
+	return (
+		<CustomLink
+			href={href}
+			style={{ backgroundImage: `url(${bgUrl})` }}
+			className="bg-cover bg-center text-secondary-foreground w-48 h-52 rounded-md -skew-x-6 relative py-4 px-3 hover:outline-4 hover:outline-accent-foreground"
+		>
+			<div className="bg-gradient-to-b from-white from-0% via-50% via-white/50 to-transparent to-75% size-full absolute inset-0 rounded-md" />
+			<p className="text-2xl font-bold skew-x-6 italic w-fit">{children}</p>
+		</CustomLink>
+	);
 };
 
 export default BigCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,44 @@
 import BigCard from "components/ui/big-card";
 
+interface IFeatures {
+	url: string;
+	bgPath: string;
+	title: string;
+}
+
+const mainFeatures: IFeatures[] = [
+	{ url: "#", bgPath: "/BG.jpg", title: "Dialog Maker" },
+	{
+		url: "#",
+		bgPath: "/BG_HyakkiyakoSquareStreet.jpg",
+		title: "Momotalk generator",
+	},
+	{ url: "#", bgPath: "/BG_SchaleOperationRoom.jpg", title: "Music Archive" },
+];
+
 export default function Page() {
-  return (
-    <div className="size-full flex flex-col gap-8 sm:gap-24 px-8 max-sm:pt-12 sm:items-center sm:justify-center">
-      <div className="flex flex-col items-center">
-        <p className="text-2xl font-bold text-secondary-foreground">Blue Archive Tools</p>
-        <p className="text-lg text-center font-semibold text-accent-foreground">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quod placeat possimus
-          dolorum nesciunt ab esse eveniet at!
-        </p>
-      </div>
-      <div className="flex flex-wrap h-fit gap-x-8 gap-y-4 sm:gap-12 justify-center">
-        <BigCard href="#" bgUrl="/BG.jpg">
-          Dialog Maker
-        </BigCard>
-        <BigCard href="#" bgUrl="/BG_HyakkiyakoSquareStreet.jpg">
-          Momotalk Generator
-        </BigCard>
-        <BigCard href="#" bgUrl="/BG_SchaleOperationRoom.jpg">
-          Music Archive
-        </BigCard>
-      </div>
-    </div>
-  );
+	return (
+		<div className="size-full flex flex-col gap-8 sm:gap-24 px-8 max-sm:pt-12 sm:items-center sm:justify-center">
+			<div className="flex flex-col items-center">
+				<p className="text-2xl font-bold text-secondary-foreground">
+					Blue Archive Tools
+				</p>
+				<p className="text-lg text-center font-semibold text-accent-foreground">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quod
+					placeat possimus dolorum nesciunt ab esse eveniet at!
+				</p>
+			</div>
+			<div className="flex flex-wrap h-fit gap-x-8 gap-y-4 sm:gap-12 justify-center">
+				{mainFeatures.map((feature) => (
+					<BigCard
+						key={feature.title}
+						bgUrl={feature.bgPath}
+						href={feature.url}
+					>
+						{feature.title}
+					</BigCard>
+				))}
+			</div>
+		</div>
+	);
 }
