40ebe1dbe3ff2c5baaed92d95df64f81778e41b8
TraFost <rahwisdilfiqrak@gmail.com>
2025-02-28 16:59:11 +0700

    feat(prisma): add Prisma schema and seed script for initial data setup

--- app/lib/prisma/prisma.ts
+++ app/lib/prisma/prisma.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-	return new PrismaClient();
+  return new PrismaClient();
 };
 
 // Declare a type for global with our custom property
 declare global {
-	var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
 // Use the global variable without shadowing globalThis
 export const prisma = global.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
-	global.prismaGlobal = prisma;
+  global.prismaGlobal = prisma;
 }
