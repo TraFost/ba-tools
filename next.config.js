25170460a791252b52617c143524566c0394ccfe
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-03-18 03:36:31 +0700

    remove empty line

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	reactStrictMode: false,
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
