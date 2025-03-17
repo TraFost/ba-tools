03dc541b6164cde1c9e278f5d0888aa54805e55e
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-03-18 03:30:33 +0700

    data dummy

---
+++ app/data/dataDummy.ts
@@ -0,0 +1,256 @@
+interface IStudent {
+  icon: string;
+  name: string;
+  description: string;
+}
+
+interface IChat {
+  icon?: string;
+  name?: string;
+  text: string;
+  role: "student" | "sensei";
+}
+
+export const students: IStudent[] = [
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    description: "Do not distrub unless its really important.",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    description: "EAT OR DIE",
+  },
+  {
+    icon: "/Akane_(Bunny_Girl).png",
+    name: "Akane",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+  },
+];
+
+export const chats: IChat[] = [
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium.",
+    role: "student",
+  },
+  {
+    text: "sensei text",
+    role: "sensei",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+    role: "student",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium.",
+    role: "student",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget dui finibus, lobortis mauris ac, volutpat augue. Nam euismod eu.",
+    role: "sensei",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium.",
+    role: "student",
+  },
+  {
+    text: "sensei text",
+    role: "sensei",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium.",
+    role: "student",
+  },
+  {
+    text: "sensei text",
+    role: "sensei",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Ako.png",
+    name: "Ako",
+    text: "Lorem ipsum dolor sit amet.",
+    role: "student",
+  },
+  {
+    icon: "/Haruna.png",
+    name: "Haruna",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium.",
+    role: "student",
+  },
+  {
+    text: "sensei text",
+    role: "sensei",
+  },
+];
