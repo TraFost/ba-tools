c60cd1219c7775efeb173e3612aeaa822f2ead55
TraFost <rahwisdilfiqrak@gmail.com>
2025-03-22 23:56:14 +0700

    refactor(config): update rewrites for API routes

--- next.config.js
+++ next.config.js
@@ -4,21 +4,6 @@ const nextConfig = {
 		ignoreDuringBuilds: true,
 	},
 	reactStrictMode: false,
-	webpack: (config) => {
-		config.externals.push({
-			"utf-8-validate": "commonjs utf-8-validate",
-			bufferutil: "commonjs bufferutil",
-		});
-		return config;
-	},
-	async rewrites() {
-		return [
-			{
-				source: "/api/auth/:path*",
-				destination: "/api/auth/:path*",
-			},
-		];
-	},
 };
 
 module.exports = nextConfig;
