216140601181d2a88eabe7c6fb6e848dde513653
TraFost <rahwisdilfiqrak@gmail.com>
2025-03-22 23:47:05 +0700

    feat: add webpack externals for utf-8-validate and bufferutil

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  reactStrictMode: false,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	reactStrictMode: false,
+	webpack: (config) => {
+		config.externals.push({
+			"utf-8-validate": "commonjs utf-8-validate",
+			bufferutil: "commonjs bufferutil",
+		});
+		return config;
+	},
 };
 
 module.exports = nextConfig;
