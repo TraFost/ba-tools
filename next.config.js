c4a8e9c93d7a2d3aba094be42649fa394f0a8485
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-05-12 19:47:30 +0700

    fix(config): unoptimized images set to true because error in production and ensure consistent formatting

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   },
   reactStrictMode: false,
   images: {
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: "https",
@@ -13,6 +14,6 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
